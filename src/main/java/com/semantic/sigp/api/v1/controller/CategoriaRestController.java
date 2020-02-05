d8c48cd80964a88da62402faa9db2933a0758847
Danilo Pereira <danilo.pereira@techlead.com.br>
2020-02-05 17:39:55 -0300

    add cors

--- src/main/java/com/semantic/sigp/api/v1/controller/CategoriaRestController.java
+++ src/main/java/com/semantic/sigp/api/v1/controller/CategoriaRestController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController()
 @RequestMapping("v1/categorias")
+@CrossOrigin("*")
 public class CategoriaRestController extends AbstractRestController<Categoria> {
 
 	@Autowired
