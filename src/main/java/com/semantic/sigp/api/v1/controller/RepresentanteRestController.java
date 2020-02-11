3653bf1d782e6a1688b200e5e52a73f255b6a30b
danilodacosta <danilo-gc@hotmail.com>
2020-02-11 00:32:34 -0300

    ajustando restController das entidades representante e comunidade.

--- src/main/java/com/semantic/sigp/api/v1/controller/RepresentanteRestController.java
+++ src/main/java/com/semantic/sigp/api/v1/controller/RepresentanteRestController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController()
 @RequestMapping("v1/representantes")
+@CrossOrigin("*")
 public class RepresentanteRestController extends AbstractRestController<Representante> {
 
 	@Autowired
