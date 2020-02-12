ae4f3ea5a0f5bacceafece9bc90a30d9c4f7cafb
danilodacosta <danilo-gc@hotmail.com>
2020-02-12 07:08:02 -0300

    visitante controller

--- src/main/java/com/semantic/sigp/api/v1/controller/VisitanteRestController.java
+++ src/main/java/com/semantic/sigp/api/v1/controller/VisitanteRestController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController()
 @RequestMapping("v1/visitantes")
+@CrossOrigin("*")
 public class VisitanteRestController extends AbstractRestController<Visitante> {
 
 	@Autowired
