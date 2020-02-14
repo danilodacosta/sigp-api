8a9a0c3c885444c06375a3f4f7f2eb187ee0411d
Danilo Pereira <danilo.pereira@techlead.com.br>
2020-02-14 20:45:55 -0200

    ajuste no endpoint do cep.

--- src/main/java/com/semantic/sigp/api/v1/controller/EnderecoWebRestController.java
+++ src/main/java/com/semantic/sigp/api/v1/controller/EnderecoWebRestController.java
@@ -2,6 +2,7 @@ package com.semantic.sigp.api.v1.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +12,8 @@ import com.semantic.sigp.api.v1.model.dto.EnderecoWeb;
 import com.semantic.sigp.api.v1.service.IConsultaCEPService;
 
 @RestController
-@RequestMapping("/v1/endereco-web/")
+@RequestMapping("v1/endereco-web/")
+@CrossOrigin("*")
 public class EnderecoWebRestController {
 
     @Autowired
